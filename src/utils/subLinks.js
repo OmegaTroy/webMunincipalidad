c8ad0f63ab707d34f930258a689b4f20fff8e9de
OmegaTroy <juli.mjoi44@gmail.com>
2024-12-11 07:22:56 -0300

    new file subLinks

---
+++ src/utils/subLinks.js
@@ -0,0 +1,50 @@
+export const subLinks = [
+  {
+    title: "Tasas Municipales",
+    description:
+      "Las tasas municipales son impuestos locales que financian servicios públicos esenciales, como recolección de basura, mantenimiento de calles y seguridad en la comunidad.",
+    url: "/tasas-municipales",
+  },
+  {
+    title: "Compras y Contrataciones",
+    description:
+      "Ser proveedor de un municipio implica ofrecer productos o servicios a la administración local, participando en licitaciones o contratos.",
+    url: "/compras-y-contrataciones",
+  },
+  {
+    title: "Licencias",
+    description:
+      "Obtener la licencia de conducir requiere cumplir con requisitos básicos: presentar documentos, aprobar un examen teórico y práctico",
+    url: "/licencias",
+  },
+  {
+    title: "Habilitaciones",
+    description:
+      "La habilitación de locales exige cumplir normas municipales, presentar documentación, inspecciones y permisos para operar de manera legal y segura.",
+    url: "/habilitaciones",
+  },
+  {
+    title: "Multas",
+    description:
+      "Las multas de tránsito son sanciones por infringir normas viales. Implican el pago de una multa y, en algunos casos, la pérdida de puntos de licencia.",
+    url: "/multas",
+  },
+  {
+    title: "Portal de Autogestion",
+    description:
+      "El Portal de Autogestión permite a los ciudadanos realizar trámites en línea, como pagos, consultas y gestiones sin necesidad de acudir personalmente.",
+    url: "/portal de autogestion",
+  },
+  {
+    title: "Tramites",
+    description:
+      "La Guía de Trámites Municipales ofrece información sobre los procedimientos, requisitos y lugares para realizar gestiones en el municipio.",
+    url: "/tramites",
+  },
+  {
+    title: "Turnos",
+    description:
+      '"Saca tus turnos" permite agendar citas para trámites, servicios o gestiones de manera rápida y organizada, sin esperas innecesarias.',
+    url: "/turnos",
+  },
+];
